cbb127b51ec8d73a3fde1c139ceb7433824e55fc
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-07-10 23:47:56 +0100

    Eutro helps Eutro

---
+++ src/main/java/eutros/tetraits/handler/ASMFieldHandler.java
@@ -0,0 +1,16 @@
+package eutros.tetraits.handler;
+
+import net.minecraft.util.ResourceLocation;
+import se.mickelus.tetra.module.data.ModuleVariantData;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class ASMFieldHandler {
+
+    @Nullable
+    public static Map<ResourceLocation, Object> getTetraitsField(ModuleVariantData data) {
+        return null; // Replaced with ASM.
+    }
+
+}
---
+++ src/main/resources/asm/attach-traits-field.js
@@ -0,0 +1,56 @@
+function initializeCoreMod() {
+    var ASM = Java.type("net.minecraftforge.coremod.api.ASMAPI");
+    var Opcodes = Java.type("org.objectweb.asm.Opcodes");
+    var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
+    var FieldInsnNode = Java.type("org.objectweb.asm.tree.FieldInsnNode");
+    var FieldNode = Java.type("org.objectweb.asm.tree.FieldNode");
+    var InsnList = Java.type("org.objectweb.asm.tree.InsnList");
+
+    var FIELD_NAME = "tetraits";
+    
+    return {
+        "add-field": {
+            "target": {
+                "type": "CLASS",
+                "name": "se.mickelus.tetra.module.data.ModuleVariantData"
+            },
+            "transformer": function (clazz) {
+                clazz.fields.add(new FieldNode(
+                    Opcodes.ACC_PUBLIC | Opcodes.ACC_SYNTHETIC,
+                    FIELD_NAME,
+                    "Ljava/util/Map;",
+                    "Ljava/util/Map<Lnet/minecraft/util/ResourceLocation;Ljava/lang/Object;>;",
+                    null
+                ));
+
+                return clazz;
+            }
+        },
+        "assemble-getter": {
+            "target": {
+                "type": "METHOD",
+                "class": "eutros.tetraits.handler.ASMFieldHandler",
+                "methodName": "getTetraitsField",
+                "methodDesc": "(Lse/mickelus/tetra/module/data/ModuleVariantData;)Ljava/util/Map;"
+            },
+            "transformer": function (method) {
+                var newInstructions = new InsnList();
+
+                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                newInstructions.add(new FieldInsnNode(
+                    Opcodes.GETFIELD,
+                    "se/mickelus/tetra/module/data/ModuleVariantData",
+                    FIELD_NAME,
+                    "Ljava/util/Map;"
+                ));
+
+                var target = ASM.findFirstInstruction(method, Opcodes.ACONST_NULL);
+
+                method.instructions.insert(target, newInstructions);
+                method.instructions.remove(target);
+
+                return method;
+            }
+        }
+    }
+}
