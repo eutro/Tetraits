b3ac70fdd6128192b9e2863e97deb35c68e6d967
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-07-25 01:35:59 +0100

    Fancy formatting and two-sided command.

--- src/main/java/eutros/tetraits/command/ReloadCommand.java
+++ src/main/java/eutros/tetraits/command/ReloadCommand.java
@@ -1,6 +1,7 @@
 package eutros.tetraits.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import eutros.tetraits.data.DataManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
@@ -8,6 +9,9 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.ClientChatEvent;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static net.minecraft.command.Commands.literal;
 
@@ -18,26 +22,28 @@ public class ReloadCommand {
                 .then(literal("reload")
                         .then(literal("server")
                                 .requires(source -> source.hasPermissionLevel(2))
-                                .executes(context -> {
-                                    context.getSource()
-                                            .sendFeedback(
-                                                    new TranslationTextComponent("tetraits.commands.reload.start"),
-                                                    true
-                                            );
-                                    DataManager.getInstance().load();
-                                    context.getSource()
-                                            .sendFeedback(
-                                                    new TranslationTextComponent("tetraits.commands.reload.finished")
-                                                            .applyTextStyle(TextFormatting.GREEN),
-                                                    true
-                                            );
-                                    return 0;
-                                }))
+                                .executes(ReloadCommand::reloadServer))
+                        .then(literal("both")
+                                .requires(source -> source.hasPermissionLevel(2))
+                                .executes(ReloadCommand::reloadServer))
                         .then(literal("client").executes(context -> 0))));
     }
 
+    private static int reloadServer(CommandContext<CommandSource> context) {
+        context.getSource()
+                .sendFeedback(new TranslationTextComponent("tetraits.commands.reload.start.server")
+                        .applyTextStyle(TextFormatting.DARK_PURPLE), true);
+        DataManager.getInstance().load();
+        context.getSource()
+                .sendFeedback(new TranslationTextComponent("tetraits.commands.reload.finished")
+                        .applyTextStyle(TextFormatting.GREEN), true);
+        return 0;
+    }
+
+    public static final Predicate<String> RELOAD_CLIENT = Pattern.compile("/tetraits reload (client|both)").asPredicate();
+
     public static void clientSend(ClientChatEvent evt) {
-        if(!"/tetraits reload client".equals(evt.getMessage())) {
+        if(!RELOAD_CLIENT.test(evt.getMessage())) {
             return;
         }
 
@@ -47,7 +53,8 @@ public class ReloadCommand {
             return;
         }
 
-        player.sendMessage(new TranslationTextComponent("tetraits.commands.reload.start"));
+        player.sendMessage(new TranslationTextComponent("tetraits.commands.reload.start.client")
+                .applyTextStyle(TextFormatting.DARK_AQUA));
         DataManager.getInstance().load();
         player.sendMessage(new TranslationTextComponent("tetraits.commands.reload.finished")
                 .applyTextStyle(TextFormatting.GREEN));
